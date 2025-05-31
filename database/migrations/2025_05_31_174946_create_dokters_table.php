2ef6dcc82aaacb0540717cfd8fe6ad921c46807e
anammaulana <anammaulana210447@gmail.com>
2025-06-01 01:02:58 +0700

    feat: buat migration

---
+++ database/migrations/2025_05_31_174946_create_dokters_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dokters', function (Blueprint $table) {
+            $table->string('id_dokter')->primary();
+            $table->string('nama');
+            $table->string('spesialisasi');
+            $table->string('no_str')->unique();
+            $table->string('jadwal_praktik');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dokters');
+    }
+};
---
+++ database/migrations/2025_05_31_174959_create_pemeriksaans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pemeriksaans', function (Blueprint $table) {
+            $table->string('id_pemeriksaan')->primary();
+            $table->string('id_pasien')->constrained('pasiens')->onDelete('cascade');
+            $table->string('id_dokter')->constrained('dokters')->onDelete('cascade');
+            $table->date('tanggal_periksa');
+            $table->text('keluhan');
+            $table->text('diagnosa')->nullable();
+            $table->text('resep_obat')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pemeriksaans');
+    }
+};
