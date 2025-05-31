8337498daca2709854d00a2c95be7dc5d69357c0
anammaulana <anammaulana210447@gmail.com>
2025-05-31 22:56:53 +0700

    add koneksi oracle

--- config/database.php
+++ config/database.php
@@ -112,6 +112,21 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'oracle' => [
+    'driver'         => 'oracle',
+    'tns'            => '',
+    'host'           => env('DB_HOST', 'localhost'),
+    'port'           => env('DB_PORT', '1521'),
+    'database'       => env('DB_SERVICE_NAME', ''),
+    'service_name'   => env('DB_SERVICE_NAME', ''),
+    'sid'            => env('DB_SID', ''),
+    'username'       => env('DB_USERNAME', ''),
+    'password'       => env('DB_PASSWORD', ''),
+    'charset'        => 'AL32UTF8',
+    'prefix'         => '',
+],
+
+
     ],
 
     /*
